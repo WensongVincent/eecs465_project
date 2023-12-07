307e0acf85ae63561950ae6f400280708dd400ed
WensongVincent <umhws@umich.edu>
2023-12-06 21:01:11 -0500

    update path and add path reading

--- demo.py
+++ demo.py
@@ -26,6 +26,33 @@ def main(screenshot=False):
 
     start_config = tuple(get_joint_positions(robots['pr2'], base_joints))
 
+    # read path
+    path = []
+    line_temp = []
+    with open('path_maze.txt', 'r') as file:
+        for line in file:
+            if ']' in line:
+                line_temp.append(line)
+                joint_line = ''.join(line_temp).replace('[', ' ').replace(']', ' ').replace('\n', ' ').split(' ')
+                joint_line = np.array([float(num) for num in joint_line if num is not ''])
+                path.append(joint_line)
+                line_temp = []
+            else:
+                line_temp.append(line)
+    path = np.array(path)
+
+    # interpolate path
+    x_before_interpolate = np.linspace(0, path.shape[1] - 1, path.shape[1])
+    x_after_interpolate = np.linspace(0, path.shape[1], 1000)
+    path_temp = []
+    for item in path:
+        path_temp.append(np.interp(x_after_interpolate, x_before_interpolate, np.squeeze(item)))
+    path = np.array(path_temp).T
+
+
+    # execute path
+    execute_trajectory(robots['pr2'], base_joints, path, sleep=0.001)
+
     wait_if_gui()
     disconnect()
 
--- pf.py
+++ pf.py
@@ -33,18 +33,28 @@ def get_sensor():
     pass
     
 def main():
+    # read path
     path = []
-    pattern = r'\[((?:\n|.)*?)\]'
+    line_temp = []
     with open('path_maze.txt', 'r') as file:
-        content = file.read()
-        matches = re.findall(pattern, content)
-        for match in matches:
-            nums_in_match = re.findall(r'(\d+\.?\d*)', match)
-            path.extend(float(num) for num in nums_in_match)
-    path = np.array(path).reshape(3,-1)
+        for line in file:
+            if ']' in line:
+                line_temp.append(line)
+                joint_line = ''.join(line_temp).replace('[', ' ').replace(']', ' ').replace('\n', ' ').split(' ')
+                joint_line = np.array([float(num) for num in joint_line if num is not ''])
+                path.append(joint_line)
+                line_temp = []
+            else:
+                line_temp.append(line)
+    path = np.array(path)
 
-    # import pdb; pdb.set_trace()
-    
+    # interpolate path
+    x_before_interpolate = np.linspace(0, path.shape[1] - 1, path.shape[1])
+    x_after_interpolate = np.linspace(0, path.shape[1], 1000)
+    path_temp = []
+    for item in path:
+        path_temp.append(np.interp(x_after_interpolate, x_before_interpolate, np.squeeze(item)))
+    path = np.array(path_temp).T
 
     t = 0
     pf = ParticleFilter()
