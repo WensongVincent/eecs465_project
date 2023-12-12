b7c49cc85c74f8fd89f28a6d8653e277ac6571c2
WensongVincent <umhws@umich.edu>
2023-12-12 01:17:26 -0500

    pf improvement

--- pf.py
+++ pf.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import scipy.stats as scistats
 import copy
 
-T_MAX = 100 # iteration for particle filter & len of interpolated path
-SENSOR_STD = [0.1, 0.1, 0.1] #[x, y, theta]
-ACTION_STD = [0.5, 0.5, 0.5] #[x, y, theta]
+T_MAX = 300 # iteration for particle filter & len of interpolated path
+ACTION_STD = [0.05, 0.05, 0.05] #[x, y, theta]
+SENSOR_STD = [0.03, 0.03, 0.03] #[x, y, theta]
 NUM_PARTICLES = 1000 # number of particles for particle filter
 X_MAX = 8 # max x length for map
 Y_MAX = 4 # max y length for map
+ACTION_ONLY = False
     
 class ParticleFilter():
     def __init__(self, num_particles, x_max, y_max) -> None:
@@ -20,8 +21,10 @@ class ParticleFilter():
         self.particles_t = []
         self.samples_t = [] #[[x, y, theta, w],...]
         self.weight_t = []
+        self.weight_tminus1 = []
         self.u_t = []
         self.z_t = []
+        self.estimated_path = []
 
     def random_init(self, x_max, y_max):
         self.particles_t0 = np.random.rand(3, self.num_particles)
@@ -46,20 +49,20 @@ class ParticleFilter():
         cov_y = np.sqrt(action_std[1] * np.abs(mean[1]))
         cov_theta = np.sqrt(action_std[2] * np.abs(mean[2]))
         
-        actual_dx = np.random.normal(mean[0], cov_x)
-        actual_dy = np.random.normal(mean[1], cov_y)
-        actual_dtheta = np.random.normal(mean[2], cov_theta)
-        actual_dxytheta = np.array([actual_dx, actual_dy, actual_dtheta])
+        actual_dx = np.random.normal(mean[0], cov_x, self.num_particles).reshape(self.num_particles, -1)
+        actual_dy = np.random.normal(mean[1], cov_y, self.num_particles).reshape(self.num_particles, -1)
+        actual_dtheta = np.random.normal(mean[2], cov_theta, self.num_particles).reshape(self.num_particles, -1)
+        actual_dxytheta = np.concatenate((actual_dx, actual_dy, actual_dtheta), axis=1)
         
         particles_t = self.particles_tminus1 + actual_dxytheta
         particles_t.T[2] = warp_to_pi(particles_t.T[2])
         self.particles_t = particles_t
     
-    def sensor_model(self, sensor_std):
+    def sensor_model(self, sensor_std, action_only):
         '''
         w_t_m = p(z_t | x_t_m)
         S_t = S_t U (x_t_m, w_t_m)
-        Update self.weight_t and self.sample_t
+        Update self.weight_t
         '''
         w_sum = 0
         self.weight_t = []
@@ -76,22 +79,44 @@ class ParticleFilter():
             w_sum += w_t_m
         self.weight_t /= w_sum
         # print(self.weight_t.reshape(-1, 1).shape)
-        self.samples_t = np.concatenate((self.particles_t, self.weight_t.reshape(-1, 1)), axis=1)
+        # self.samples_t = np.concatenate((self.particles_t, self.weight_t.reshape(-1, 1)), axis=1)
         
-    def low_var_resample(self):
+        # self.estimated_path.append(self.particles_t[self.weight_t.argmax(), :])
+        
+        
+    def low_var_resample(self, action_only):
         '''
-        Update self.particle_tminus1
+        Update self.samplt_t, self.particle_tminus1, self.weight_tminus1
         '''
         self.particles_tminus1 = np.zeros((self.num_particles, 3))
+        self.samples_t = np.zeros((self.num_particles, 4))
+        weight_sum = 0
+        
         r = np.random.uniform(0, 1.0 / self.num_particles)
-        c = self.samples_t[0, 3]
+        c = self.weight_t[0]
         i = 0
         for m in range(self.num_particles):
             U = r + m * (1 / self.num_particles)
-            while U > c:
+            while U > c and i < self.num_particles - 1:
                 i += 1
-                c += self.samples_t[i, 3]
-            self.particles_tminus1[m] = self.samples_t[i, :3]
+                c += self.weight_t[i]
+            # self.particles_tminus1[m] = self.particles_t[i, :]
+            # import pdb; pdb.set_trace()
+            self.samples_t[m,:] = copy.deepcopy(np.concatenate((self.particles_t[i, :].reshape(1,-1), self.weight_t[i].reshape(1,-1)), axis=1))
+        self.samples_t[:, 3] /= self.samples_t[:, 3].sum()
+        self.particles_tminus1 = copy.deepcopy(self.samples_t[:,:3])
+        self.weight_tminus1 = copy.deepcopy(self.samples_t[:,3])
+        
+    
+    def estimate_config(self):
+        x = (self.samples_t[:, 0] * self.samples_t[:, 3]).sum()
+        y = (self.samples_t[:, 1] * self.samples_t[:, 3]).sum()
+        theta_cos = (np.cos(self.samples_t[:, 2]) * self.samples_t[:, 3]).sum()
+        theta_sin = (np.sin(self.samples_t[:, 2]) * self.samples_t[:, 3]).sum()
+        theta = np.arctan2(theta_cos, theta_sin)
+        
+        self.estimated_path.append(np.array([x, y, theta]))
+            
     
 def get_action(path: np.ndarray, t) -> np.ndarray:
     '''
@@ -155,7 +180,7 @@ def main():
     for item in path:
         path_temp.append(np.interp(x_after_interpolate, x_before_interpolate, np.squeeze(item)))
     path = np.array(path_temp).T
-    print(path.shape)
+    # print(path.shape)
     
     
     ################ particle filter ################
@@ -169,7 +194,6 @@ def main():
     
     while(t < T_MAX - 1):  
         t += 1
-        print(f"Num of iteration: {t}")
         
         # get control input and sensor data
         pf.u_t, moved = get_action(path, t)
@@ -186,32 +210,45 @@ def main():
             
             # apply action model
             pf.action_model(ACTION_STD)
+            # plt.figure(1)
+            # plt.scatter(pf.particles_t.T[0], pf.particles_t.T[1], s=5)
+            # plt.show()
             
             # apply sensor model
-            pf.sensor_model(SENSOR_STD)
+            pf.sensor_model(SENSOR_STD, ACTION_ONLY)
             
             # apply resampling
-            pf.low_var_resample()
+            pf.low_var_resample(ACTION_ONLY)
+            # plt.figure(2)
+            # plt.scatter(pf.particles_tminus1.T[0], pf.particles_tminus1.T[1], s=5)
+            # plt.show
             
-            if t%10 == 0:
+            pf.estimate_config()
+            
+            if t == 0 or t%30 == 0 or t == T_MAX-1:
                 particles_cache.append(pf.particles_t)
+                print(f"Num of iteration: {t}")
 
     ################ plotting ################
     plt.figure(1)
-    plt.scatter(path.T[0], path.T[1], s=5)
-    plt.scatter(np.array(z_cache).T[0], np.array(z_cache).T[1], s=5)
-
+    plt.plot(path.T[0], path.T[1], label='Desired Path', c='b')
+    plt.scatter(np.array(z_cache).T[0], np.array(z_cache).T[1], s=5, label='Sensor Measurement', c='g')
+    # plt.plot(np.array(pf.estimated_path).T[0], np.array(pf.estimated_path).T[1], linestyle = '--',label='Estimated Path')
+    plt.scatter(np.array(pf.estimated_path).T[0], np.array(pf.estimated_path).T[1], s=5 ,label='Estimated Path', c='r')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.title('PF desired path, sensor measurement and estimated path')
+    
     plt.figure(2)
-    plt.scatter(pf.particles_t0.T[0], pf.particles_t0.T[1], s=5)
-    
-    plt.figure(3)
-    plt.scatter(particles_cache[0].T[0], particles_cache[0].T[1], s=5)
-    
-    plt.figure(4)
-    plt.scatter(particles_cache[1].T[0], particles_cache[1].T[1], s=5)
-    
-    plt.figure(5)
-    plt.scatter(particles_cache[2].T[0], particles_cache[2].T[1], s=5)
+    plt.plot(path.T[0], path.T[1], label='Desired Path', c='b')
+    plt.scatter(np.array(z_cache).T[0], np.array(z_cache).T[1], s=5, label='Sensor Measurement', c='g')
+    plt.scatter(np.array(particles_cache).T[0], np.array(particles_cache).T[1], s=5 ,label='Estimated Path', c='r')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
 
     plt.show()
 
