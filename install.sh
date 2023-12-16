f3c62fac15d9a4ba98024f2f89103f957d67b598
WensongVincent <umhws@umich.edu>
2023-12-15 21:49:04 -0500

    update

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 # Install dependencies
 echo "Installing dependencies..."
 
-pip install matplotlib
-pip install numpy
-pip install psutil
-pip install pybullet
-pip install PyYAML
+pip install matplotlib==3.7.3
+pip install numpy==1.23.5
+pip install psutil==5.9.6
+pip install pybullet==3.2.5
+pip install PyYAML==6.0.1
 pip install scipy
-pip install tqdm
+pip install tqdm==4.66.1
 
 echo "All dependencies have been installed."
